7a8f4dd72db5f5b6cce86ccf4951169ab0dd9394
Jakob Garde <jakob@rigsters.com>
2025-04-18 21:13:51 +0200

    code cleanup

--- main_wireframe.cpp
+++ main_wireframe.cpp
@@ -125,12 +125,19 @@ void RenderPoint(u8 *image_buffer, Vector3f point_ndc, u32 w, u32 h, Color color
     ((Color*) image_buffer)[ GetXYIdx(x, y, w) ] = color;
 }
 
-f32 PointPlaneSign(Vector3f point, Ray plane) {
+bool PointSideOfPlane(Vector3f point, Ray plane) {
+    // returns true if point is in the R3-halfspace defined by plane normal
+
     Vector3f diff = (plane.position - point);
     diff.Normalize();
-    f32 result = diff.Dot(plane.direction);
+    f32 cos_angle = diff.Dot(plane.direction);
 
-    return result;
+    if (cos_angle <= 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 void RenderFatPoint3x3(u8 *image_buffer, Matrix4f view, Matrix4f proj, Vector3f point, u32 w, u32 h, Color color = COLOR_RED) {
@@ -139,11 +146,9 @@ void RenderFatPoint3x3(u8 *image_buffer, Matrix4f view, Matrix4f proj, Vector3f 
     Ray view_plane = { Vector3f { 0, 0, 0.1 }, Vector3f { 0, 0, 1 } };
     Ray view_plane_far = { Vector3f { 0, 0, 1 }, Vector3f { 0, 0, 1 } };
 
-    f32 sign = PointPlaneSign(point_cam, view_plane);
-    if (sign > 0) {
+    if (PointSideOfPlane(point_cam, view_plane) == false) {
         return;
     }
-
     Vector3f point_ndc = TransformPerspective(proj, point_cam);
 
     f32 x = (point_ndc.x + 1) / 2 * w;
@@ -169,35 +174,34 @@ void RenderLineSegment(u8 *image_buffer, Matrix4f view, Matrix4f proj, Vector3f 
 
     Ray view_plane = { Vector3f { 0, 0, 0.1 }, Vector3f { 0, 0, 1 } };
 
-    f32 sign1 = PointPlaneSign(p1_cam, view_plane);
-    f32 sign2 = PointPlaneSign(p2_cam, view_plane);
-    if (sign1 > 0 && sign2 > 0) {
-        return;
-    }
-    else if (sign1 > 0 && sign2 < 0) {
-        Ray segment = { p2_cam, p1_cam - p2_cam };
-        f32 t = 0;
-        p1_cam = RayPlaneIntersect(segment, view_plane.position, view_plane.direction, &t);
-    }
-    else if (sign1 < 0 && sign2 > 0) {
-        Ray segment = { p1_cam, p2_cam - p1_cam };
-        f32 t = 0;
-        p2_cam = RayPlaneIntersect(segment, view_plane.position, view_plane.direction, &t);
-    }
+    bool visible1 = PointSideOfPlane(p1_cam, view_plane);
+    bool visible2 = PointSideOfPlane(p2_cam, view_plane);
 
-    Vector3f p1_ndc = TransformPerspective(proj, p1_cam);
-    Vector3f p2_ndc = TransformPerspective(proj, p2_cam);
+    if (visible1 == true || visible2 == true) {
+        if (visible1 == false && visible2 == true) {
+            Ray segment = { p2_cam, p1_cam - p2_cam };
+            f32 t = 0;
+            p1_cam = RayPlaneIntersect(segment, view_plane.position, view_plane.direction, &t);
+        }
+        else if (visible1 == true && visible2 == false) {
+            Ray segment = { p1_cam, p2_cam - p1_cam };
+            f32 t = 0;
+            p2_cam = RayPlaneIntersect(segment, view_plane.position, view_plane.direction, &t);
+        }
 
-    Vector2f a = {};
-    a.x = (p1_ndc.x + 1) / 2 * w;
-    a.y = (p1_ndc.y + 1) / 2 * h;
-    Vector2f b = {};
-    b.x = (p2_ndc.x + 1) / 2 * w;
-    b.y = (p2_ndc.y + 1) / 2 * h;
+        Vector3f p1_ndc = TransformPerspective(proj, p1_cam);
+        Vector3f p2_ndc = TransformPerspective(proj, p2_cam);
 
-    RenderLineRGBA(image_buffer, w, h, a.x, a.y, b.x, b.y, color);
+        Vector2f a = {};
+        a.x = (p1_ndc.x + 1) / 2 * w;
+        a.y = (p1_ndc.y + 1) / 2 * h;
+        Vector2f b = {};
+        b.x = (p2_ndc.x + 1) / 2 * w;
+        b.y = (p2_ndc.y + 1) / 2 * h;
+
+        RenderLineRGBA(image_buffer, w, h, a.x, a.y, b.x, b.y, color);
+    }
 }
-
 
 inline
 s32 _GetNextNonDisabledWireframeIndex(u32 idx_prev, Array<Wireframe> wireframes) {
@@ -215,12 +219,11 @@ s32 _GetNextNonDisabledWireframeIndex(u32 idx_prev, Array<Wireframe> wireframes)
     }
     return idx_prev;
 }
-
-
 void RenderLineSegmentList(u8 *image_buffer, Matrix4f view, Matrix4f proj, u32 w, u32 h, Array<Wireframe> wireframes, Array<Vector3f> segments) {
     if (wireframes.len == 0) {
         return;
     }    
+    Ray view_plane = { Vector3f { 0, 0, 0.1 }, Vector3f { 0, 0, 1 } };
 
     // set up wireframe properties
     s32 wf_segs_idx = 0;
@@ -231,10 +234,35 @@ void RenderLineSegmentList(u8 *image_buffer, Matrix4f view, Matrix4f proj, u32 w
     u32 wf_nsegments = wireframes.arr[wf_idx].nsegments;
 
     for (u32 i = 0; i < segments.len / 2; ++i) {
-        Vector3f p1 = segments.arr[2*i];
-        Vector3f p2 = segments.arr[2*i + 1];
+        Vector3f p1_cam = TransformInversePoint(view, segments.arr[2*i]);
+        Vector3f p2_cam = TransformInversePoint(view, segments.arr[2*i + 1]);
 
-        RenderLineSegment(image_buffer, view, proj, p1, p2, w, h, wf_color);
+        bool visible1 = PointSideOfPlane(p1_cam, view_plane);
+        bool visible2 = PointSideOfPlane(p2_cam, view_plane);
+
+        if (visible1 == true || visible2 == true) {
+            if (visible1 == false && visible2 == true) {
+                Ray segment = { p2_cam, p1_cam - p2_cam };
+                f32 t = 0;
+                p1_cam = RayPlaneIntersect(segment, view_plane.position, view_plane.direction, &t);
+            }
+            else if (visible1 == true && visible2 == false) {
+                Ray segment = { p1_cam, p2_cam - p1_cam };
+                f32 t = 0;
+                p2_cam = RayPlaneIntersect(segment, view_plane.position, view_plane.direction, &t);
+            }
+            Vector3f p1_ndc = TransformPerspective(proj, p1_cam);
+            Vector3f p2_ndc = TransformPerspective(proj, p2_cam);
+
+            Vector2f a = {};
+            a.x = (p1_ndc.x + 1) / 2 * w;
+            a.y = (p1_ndc.y + 1) / 2 * h;
+            Vector2f b = {};
+            b.x = (p2_ndc.x + 1) / 2 * w;
+            b.y = (p2_ndc.y + 1) / 2 * h;
+
+            RenderLineRGBA(image_buffer, w, h, a.x, a.y, b.x, b.y, wf_color);
+        }
 
         // update object-specific properties s.a. colour, style
         wf_segs_idx++;
