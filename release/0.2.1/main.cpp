e783426d7770afdbf6ed620b384f23ef265c8748
Jakob Garde <jakob@rigsters.com>
2025-07-26 06:53:55 +0200

    compiled the cbui version 0.2.1; includes changes that came out of the testris project

---
+++ release/0.2.1/main.cpp
@@ -0,0 +1,11 @@
+// #define ENABLE_GLFW
+
+#include "jg_cbui.h"
+
+
+int main (int argc, char **argv) {
+    printf("Compile and version check for cbui %u.%u.%u\n", CBUI_VERSION_MAJOR, CBUI_VERSION_MINOR, CBUI_VERSION_PATCH);
+
+    BaselayerAssertVersion(0, 2, 3);
+    CbuiAssertVersion(0, 2, 1);
+}
